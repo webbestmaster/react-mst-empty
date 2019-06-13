6b24cc26a35b60c82ee6590896dc8b22f2eef715
turovtsov <dturovtsov@marathonbet.ru>
2019-06-13 18:24:29 +0300

    just begin

--- www/js/component/app/c-app.js
+++ www/js/component/app/c-app.js
@@ -4,22 +4,29 @@ import type {Node} from 'react';
 import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
-import {ReduxStoreProvider} from '../../redux-store-provider/provider';
-import {System} from '../system/c-system';
-import {Auth} from '../auth/c-auth';
-import {Home} from '../../page/home/c-home';
-import {appConst} from '../../const';
-import {Login} from '../../page/login/c-login';
-import {userIsAuthenticated, userIsNotAuthenticated} from '../auth/auth-helper';
-import {PageNotFound} from '../../page/page-not-found/c-page-not-found';
+// import {ReduxStoreProvider} from '../../redux-store-provider/provider';
+// import {System} from '../system/c-system';
+// import {Auth} from '../auth/c-auth';
+// import {Home} from '../../page/home/c-home';
+// import {appConst} from '../../const';
+// import {Login} from '../../page/login/c-login';
+// import {userIsAuthenticated, userIsNotAuthenticated} from '../auth/auth-helper';
+// import {PageNotFound} from '../../page/page-not-found/c-page-not-found';
 
 import {routes} from './routes';
 
-console.log(appConst);
+// console.log(appConst);
 
 export function App(): Node {
     return (
-        /* eslint-disable react/jsx-max-depth */
+        <div>!</div>
+    );
+}
+
+/*
+export function App(): Node {
+    return (
+        /!* eslint-disable react/jsx-max-depth *!/
         // you can replace the extra <div> with any react component
         <ReduxStoreProvider>
             <>
@@ -37,6 +44,7 @@ export function App(): Node {
                 </System>
             </>
         </ReduxStoreProvider>
-        /* eslint-enable react/jsx-max-depth */
+        /!* eslint-enable react/jsx-max-depth *!/
     );
 }
+*/
